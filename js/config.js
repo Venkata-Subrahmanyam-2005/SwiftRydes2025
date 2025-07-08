a45aa4c9b873fa724f4d1aed1ae8e076035f2291
Venkata-Subrahmanyam-2005 <venkatsubbu808@gmail.com>
2025-07-08 10:29:36 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_JNd1H53ur', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '8a04g25mqfqh7889ored8ksq0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'eu-north-1_zuTyHVZ1x', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3thgm5oiqdtpu9i0h280bf4pqu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
